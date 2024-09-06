7fa8ed561cd63b458b4e3d544bd7c37f095a6dbb
erfanpv <erfanpv786@gmail.com>
2024-09-06 21:26:15 +0530

    Handle Products

--- src/Pages/ProductStore/Products.jsx
+++ src/Pages/ProductStore/Products.jsx
@@ -5,28 +5,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../app/Slice/ProductsSlice/productThunk.js";
 
 const Products = () => {
-  const { products, isLoading} = useSelector((state) => state.productsAll);
+  const { products, isLoading } = useSelector((state) => state.productsAll);
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchProducts());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center mt-10 mx-5">
-        {products?.length === 0 || isLoading === true 
-          ? [...Array(6)].map((_, index) => <ProductShimmer key={index} />)
-          : products?.map((productItem) => {
-              return (
-                <FurnitureCard
-                  productItem={productItem}
-                  key={productItem._id}
-                />
-              );
-            })}
+    <div className="mt-10 mx-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
+        {isLoading ? (
+          [...Array(6)].map((_, index) => <ProductShimmer key={index} />)
+        ) : products?.length === 0 ? (
+          <div className="col-span-full text-center text-gray-500">
+            No products found.
+          </div>
+        ) : (
+          products.map((productItem) => (
+            <FurnitureCard productItem={productItem} key={productItem._id} />
+          ))
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
