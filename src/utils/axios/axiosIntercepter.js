439f7ec10232bcb5ff4adcea99483802c038dfc1
erfanpv <erfanpv786@gmail.com>
2024-09-08 23:45:47 +0530

    Implement axios Interceptor

--- src/utils/axios/axiosIntercepter.js
+++ src/utils/axios/axiosIntercepter.js
@@ -1,4 +1,28 @@
 import axios from "axios";
-import toast from 'react-hot-toast';
 
+let baseUrl = import.meta.env.VITE_USER_API;
+const http = axios.create({ baseURL: baseUrl });
 
+http.interceptors.request.use(
+  (request) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      request.headers["authorization"] = `${token}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+http.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default http;
