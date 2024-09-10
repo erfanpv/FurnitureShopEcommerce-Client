64b55932e0926e64e45284216050a3f7c5d49109
erfanpv <erfanpv786@gmail.com>
2024-09-10 21:54:31 +0530

    Redux Store

--- src/app/Store/Store.js
+++ src/app/Store/Store.js
@@ -2,12 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../Slice/addCartSlice/addCartSlice";
 import productSlice from "../Slice/ProductsSlice/ProductSlice";
 import usersSlice from "../Slice/usersSlice/usersSlice";
+import wishListSlice from "../Slice/wishListSlice/wishListSlice";
+import dashBoardSlices from "../Slice/adminSlices/dashBoardSlices/dashBoardSlices";
+import adminProductSlices from "../Slice/adminSlices/productSlices/adminProductSlices";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
     productsAll: productSlice,
     users: usersSlice,
+    wishList: wishListSlice,
+    dashboard: dashBoardSlices,
+    adminProducts: adminProductSlices,
   },
 });
 
