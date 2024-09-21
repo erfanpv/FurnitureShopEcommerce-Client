94bfb53c1065347dd362ccf781d2b1e013d13cc4
erfanpv <erfanpv786@gmail.com>
2024-09-21 16:03:15 +0530

    Handle OrderDatails

--- src/Components/OrderDetails/OrderDetails.jsx
+++ src/Components/OrderDetails/OrderDetails.jsx
@@ -1,71 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { ordersDataByUser } from "../../app/Slice/adminSlices/ordersSlices/ordersThunk";
 
 const OrderDetails = () => {
   const { id } = useParams();
-  const [orderData, setOrderData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
+  const dispatch = useDispatch();
+  const { orderDetailsByUser, isLoading } = useSelector((state) => state.orders);
+  
   useEffect(() => {
-    const fetchOrderDetails = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/users/${id}`);
-        setOrderData(response.data.orderData);
-      } catch (error) {
-        console.error('Error fetching order details:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    dispatch(ordersDataByUser({ id }));
+  }, [id, dispatch]);
 
-    fetchOrderDetails();
-  }, [id]);
-
-  if (loading) {
-    return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-xl font-semibold text-gray-600">Loading...</p>
+      </div>
+    );
   }
 
-  if (!orderData || orderData.length === 0) {
+  if (!orderDetailsByUser || orderDetailsByUser.length === 0) {
     return (
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center h-screen">
         <div className="text-center">
           <h1 className="text-3xl font-bold text-indigo-900 mb-4">No Orders Found</h1>
-          <p className="text-gray-500">Not placed any orders yet.</p>
+          <p className="text-gray-500">You have not placed any orders yet.</p>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="p-4 ">
-      <h1 className="text-3xl font-bold mb-4 text-indigo-900">Order Details</h1>
-      {orderData.map((order, index) => (
-        <div key={index} className="mb-8 p-6 bg-white shadow-lg rounded-lg border-l-4 border-indigo-600">
-          <div className="mb-4">
-            <p><strong className="text-indigo-900">Email:</strong> {order.email}</p>
-            <p><strong className="text-indigo-900">Card Holder:</strong> {order.cardHolder}</p>
-            <p><strong className="text-indigo-900">Billing Address:</strong> {order.billingAddress}</p>
-            <p><strong className="text-indigo-900">Total:</strong> ${order.total.toFixed(2)}</p>
-            <p><strong className="text-indigo-900">Order Date:</strong> {new Date(order.orderDate).toLocaleString()}</p>
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-4xl font-extrabold text-indigo-900 mb-8">Order Details</h1>
+      <div className="grid gap-8">
+        {orderDetailsByUser.map((order) => (
+          <div
+            key={order._id}
+            className="p-6 bg-white shadow-lg rounded-lg border border-gray-300"
+          >
+            <div className="space-y-4">
+              <div className="bg-gray-50 p-4 rounded-lg border border-gray-200">
+              <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Order ID:</strong> {order.orderId}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Payment ID:</strong> {order.paymentId}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Customer Name:</strong> {order.orderedUserName}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Order Date:</strong> {new Date(order.createdAt).toLocaleDateString()}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Order Email:</strong> {order.orderUsermail}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Total Amount:</strong> ${order.total.toFixed(2)}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Payment Method:</strong> {order.payment_method}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Order Status:</strong> {order.status}
+                </p>
+                <p className="text-lg font-medium text-gray-700">
+                  <strong className="text-gray-600">Shipping Address:</strong>{" "}
+                  {`${order.shippingAddress.line1}${order.shippingAddress.line2 ? ', ' + order.shippingAddress.line2 : ''}, ${order.shippingAddress.city}, ${order.shippingAddress.state}, ${order.shippingAddress.country}, ${order.shippingAddress.postal_code}`}
+                </p>
+              </div>
+
+              <div>
+                <h3 className="text-xl font-semibold mt-6 mb-3 text-gray-800 border-b pb-2">Items Ordered</h3>
+                <ul>
+                  {order.products.map((item) => (
+                    <li key={item.productId._id} className="flex items-center mb-6 border-b border-gray-200 pb-4">
+                      <img
+                        src={item.productId.image}
+                        alt={item.productId.productName}
+                        className="w-20 h-20 object-cover rounded-lg mr-4"
+                      />
+                      <div>
+                        <h5 className="text-lg font-semibold text-gray-800">{item.productId.productName}</h5>
+                        <p className="text-sm text-gray-600">Qty: {item.quantity}</p>
+                        <p className="text-sm text-gray-600">Price: ${item.productId.price.toFixed(2)}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
-          <h2 className="text-xl font-semibold mb-2 text-rose-600">Items</h2>
-          <ul className="space-y-4">
-            {order.cartItems.map(item => (
-              <li key={item.id} className="flex items-center space-x-4 p-4 border rounded-lg">
-                <img src={item.src} alt={item.name} className="w-24 h-24 object-cover rounded-lg" />
-                <div>
-                  <p className="font-bold text-lg text-indigo-900">{item.name}</p>
-                  <p className="text-sm text-gray-600">{item.type}</p>
-                  <p className="text-gray-800">{item.description}</p>
-                  <p><strong className="text-indigo-900">Price:</strong> ${item.price}</p>
-                  <p><strong className="text-indigo-900">Quantity:</strong> {item.qty}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
