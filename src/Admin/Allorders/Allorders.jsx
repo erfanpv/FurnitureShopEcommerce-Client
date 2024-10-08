a2fdd99909be4c0c6605cb6675f95e2edeb786d9
erfanpv <erfanpv786@gmail.com>
2024-10-08 22:21:19 +0530

    Arrange allorders

--- src/Admin/Allorders/Allorders.jsx
+++ src/Admin/Allorders/Allorders.jsx
@@ -2,41 +2,106 @@ import React, { useEffect, useState } from "react";
 import { getAllordersData } from "../../app/Slice/adminSlices/ordersSlices/ordersThunk";
 import { useDispatch, useSelector } from "react-redux";
 import Modal from "react-modal";
+import CancelOrReturn from "./CancelOrReturn/CancelOrReturn.jsx";
 
 const UserOrders = () => {
   const dispatch = useDispatch();
-  const { ordersDetails } = useSelector((state) => state.orders);
-  const [selectedOrder, setSelectedOrder] = useState(null);
+  const { ordersDetails, isLoading, error, pagination } = useSelector(
+    (state) => state.orders
+  );
+  const [selectedOrderDetails, setSelectedOrderDetails] = useState(null);
+
+  const [selectedStatus, setSelectedStatus] = useState("All");
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const ordersPerPage = 10;
 
   useEffect(() => {
-    dispatch(getAllordersData());
-  }, [dispatch]);
+    dispatch(
+      getAllordersData({
+        page: currentPage,
+        limit: ordersPerPage,
+        status: selectedStatus,
+      })
+    );
+  }, [dispatch, currentPage, selectedStatus]);
 
-  const openModal = (order) => {
-    setSelectedOrder(order);
+  const openDetailsModal = (order) => {
+    setSelectedOrderDetails(order);
   };
 
-  const closeModal = () => {
-    setSelectedOrder(null);
+  const closeDetailsModal = () => {
+    setSelectedOrderDetails(null);
   };
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  if (isLoading) {
+    return <div className="text-center py-20">Loading orders...</div>;
+  }
+  if (error) {
+    return (
+      <div className="pb-20 px-5 sm:ml-64">
+        <h1 className="text-4xl font-bold text-center mb-10 text-indigo-800">
+          All Order Details
+        </h1>
+        <div className="flex justify-end mb-5">
+          <select
+            value={selectedStatus}
+            onChange={(e) => setSelectedStatus(e.target.value)}
+            className="border p-2 rounded-md"
+          >
+            <option value="All">All</option>
+            <option value="Placed">Placed</option>
+            <option value="Processing">Processing</option>
+            <option value="Shipped">Shipped</option>
+            <option value="Delivered">Delivered</option>
+            <option value="Cancelled">Cancelled</option>
+            <option value="Returned">Returned</option>
+            <option value="Refunded">Refunded</option>
+          </select>
+        </div>
+        <div className="text-center py-20">No Orders Found</div>;
+      </div>
+    );
+  }
 
   return (
     <div className="pb-20 px-5 sm:ml-64">
       <h1 className="text-4xl font-bold text-center mb-10 text-indigo-800">
         All Order Details
       </h1>
+
+      <div className="flex justify-end mb-5">
+        <select
+          value={selectedStatus}
+          onChange={(e) => setSelectedStatus(e.target.value)}
+          className="border p-2 rounded-md"
+        >
+          <option value="All">All</option>
+          <option value="Placed">Placed</option>
+          <option value="Processing">Processing</option>
+          <option value="Shipped">Shipped</option>
+          <option value="Delivered">Delivered</option>
+          <option value="Cancelled">Cancelled</option>
+          <option value="Returned">Returned</option>
+          <option value="Refunded">Refunded</option>
+        </select>
+      </div>
+
       <div className="overflow-x-auto rounded-lg shadow-md">
         <table className="min-w-full bg-white border border-gray-200 rounded-md">
           <thead>
             <tr>
               {[
+                "No",
                 "Customer",
                 "Order Date",
                 "Total",
-                "Items",
-                "Billing Address",
+                "Payment Method",
                 "Order Status",
                 "Actions",
+                "Details",
               ].map((heading) => (
                 <th
                   key={heading}
@@ -48,135 +113,136 @@ const UserOrders = () => {
             </tr>
           </thead>
           <tbody>
-            {ordersDetails.map((user) =>
-              user.orderDetails.map((order) => (
-                <tr
-                  key={order._id}
-                  className="hover:bg-gray-100 transition duration-200"
-                >
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    {order.orderedUserName}
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    {new Date(order.createdAt).toLocaleDateString()}
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    ${order.total.toFixed(2)}
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    <ul>
-                      {order.products.map((item) => (
-                        <li
-                          key={item?.productId?._id}
-                          className="flex items-center mb-4"
-                        >
-                          <img
-                            src={item?.productId?.image}
-                            alt={item?.productId?.productName}
-                            className="w-16 h-16 object-cover rounded-lg mr-4"
-                          />
-                          <div>
-                            <h5 className="text-lg font-semibold">
-                              {item?.productId?.productName}
-                            </h5>
-                            <p className="text-sm text-gray-600">
-                              Qty: {item?.quantity}
-                            </p>
-                            <p className="text-sm text-gray-600">
-                              Price: ${item?.productId?.price.toFixed(2)}
-                            </p>
-                          </div>
-                        </li>
-                        
-                        ))}
-                    </ul>
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    {`${order.shippingAddress.line1},${order.shippingAddress.line2}, ${order.shippingAddress.city}, ${order.shippingAddress.postal_code}`}
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    {order.status}
-                  </td>
-                  <td className="py-4 px-6 border-b border-gray-200">
-                    <button
-                      onClick={() => openModal(order)}
-                      className="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700"
-                    >
-                      View Details
-                    </button>
-                  </td>
-                </tr>
-              ))
-            )}
+            {ordersDetails?.map((order, index) => (
+              <tr
+                key={order.orderDetails._id}
+                className="hover:bg-gray-100 transition duration-200"
+              >
+                <td className="py-4 px-6 border-b border-gray-200">
+                  {index + 1}
+                </td>
+                <td className="py-4 px-6 border-b border-gray-200">
+                  {order?.orderDetails.orderedUserName}
+                </td>
+                <td className="py-4 px-6 border-b border-gray-200">
+                  {new Date(order.orderDetails.createdAt).toLocaleDateString()}
+                </td>
+                <td className="py-4 px-6 border-b border-gray-200">
+                  ${order.orderDetails.total.toFixed(2)}
+                </td>
+                <td className="py-4 px-6 border-b border-gray-200">
+                  {order.orderDetails.payment_method}
+                </td>
+                <td className="py-4 px-6 border-b border-gray-200">
+                  {order.orderDetails.status}
+                </td>
+                <CancelOrReturn order={order} currentPage={currentPage} ordersPerPage={ordersPerPage} />
+                <td className="py-4 px-6 border-b border-gray-200">
+                  <button
+                    onClick={() => openDetailsModal(order.orderDetails)}
+                    className="bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-700"
+                  >
+                    View Details
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
-      {/* Modal for Detailed View */}
-      {selectedOrder && (
+      {pagination?.totalPages > 1 && (
+        <div className="flex justify-center mt-6">
+          <ul className="inline-flex -space-x-px">
+            {[...Array(pagination.totalPages).keys()].map((number) => (
+              <li key={number} className="px-2">
+                <button
+                  onClick={() => paginate(number + 1)}
+                  className={`px-3 py-2 leading-tight border ${
+                    currentPage === number + 1
+                      ? "bg-indigo-600 text-white"
+                      : "bg-white text-gray-700"
+                  } rounded-md`}
+                >
+                  {number + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {selectedOrderDetails && (
         <Modal
-          isOpen={!!selectedOrder}
-          onRequestClose={closeModal}
+          isOpen={!!selectedOrderDetails}
+          onRequestClose={closeDetailsModal}
           ariaHideApp={false}
-          className="relative p-8 bg-white rounded-lg shadow-lg mx-auto mt-10 max-w-2xl"
+          className="relative p-4 sm:p-8 bg-white rounded-lg shadow-lg mx-auto mt-4 sm:mt-10 max-w-full sm:max-w-2xl max-h-[96vh] overflow-y-auto"
           overlayClassName="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center"
         >
-          <h2 className="text-2xl font-bold mb-6 text-gray-700 border-b pb-3">
+          <h2 className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6 text-gray-700 border-b pb-3">
             Order Details
           </h2>
-          <div className="space-y-4">
+
+          <div className="space-y-4 text-sm sm:text-base">
             <p>
               <strong className="text-gray-600">Order ID:</strong>{" "}
-              {selectedOrder.orderId}
+              {selectedOrderDetails.orderId}
             </p>
             <p>
               <strong className="text-gray-600">Payment ID:</strong>{" "}
-              {selectedOrder.paymentId}
+              {selectedOrderDetails.paymentId}
             </p>
             <p>
               <strong className="text-gray-600">Customer Name:</strong>{" "}
-              {selectedOrder.orderedUserName}
+              {selectedOrderDetails.orderedUserName}
             </p>
             <p>
               <strong className="text-gray-600">Order Date:</strong>{" "}
-              {new Date(selectedOrder.createdAt).toLocaleDateString()}
+              {new Date(selectedOrderDetails.createdAt).toLocaleDateString()}
             </p>
             <p>
               <strong className="text-gray-600">Total Amount:</strong> $
-              {selectedOrder.total.toFixed(2)}
+              {selectedOrderDetails.total.toFixed(2)}
             </p>
             <p>
               <strong className="text-gray-600">Payment Method:</strong>{" "}
-              {selectedOrder.payment_method}
+              {selectedOrderDetails.payment_method}
             </p>
+
             <p>
               <strong className="text-gray-600">Order Status:</strong>{" "}
-              {selectedOrder.status}
+              {selectedOrderDetails.status}
             </p>
+
             <p>
               <strong className="text-gray-600">Shipping Address:</strong>{" "}
-              {`${selectedOrder.shippingAddress.line1},${selectedOrder.shippingAddress.line2}, ${selectedOrder.shippingAddress.city}, ${selectedOrder.shippingAddress.state}, ${selectedOrder.shippingAddress.country}, ${selectedOrder.shippingAddress.postal_code}`}
+              {`${selectedOrderDetails.shippingAddress.line1}, ${selectedOrderDetails.shippingAddress.line2}, ${selectedOrderDetails.shippingAddress.city}, ${selectedOrderDetails.shippingAddress.state}, ${selectedOrderDetails.shippingAddress.country}, ${selectedOrderDetails.shippingAddress.postal_code}`}
             </p>
           </div>
 
-          {/* List of Products */}
-          <h3 className="text-lg font-semibold mt-6 mb-3 border-b pb-1">
+          <h3 className="text-lg sm:text-lg font-semibold mt-6 mb-3 border-b pb-1">
             Items Ordered
           </h3>
           <ul>
-            {selectedOrder.products.map((item) => (
-              <li key={item.productId._id} className="flex items-center mb-4">
+            {selectedOrderDetails.products.map((item, index) => (
+              <li
+                key={item.productId._id || index}
+                className="flex items-center mb-4"
+              >
                 <img
                   src={item.productId.image}
                   alt={item.productId.productName}
-                  className="w-16 h-16 object-cover rounded-lg mr-4"
+                  className="w-14 h-14 sm:w-16 sm:h-16 object-cover rounded-lg mr-4"
                 />
                 <div>
-                  <h5 className="text-lg font-semibold text-gray-800">
+                  <h5 className="text-sm sm:text-lg font-semibold text-gray-800">
                     {item.productId.productName}
                   </h5>
-                  <p className="text-sm text-gray-600">Qty: {item.quantity}</p>
-                  <p className="text-sm text-gray-600">
+                  <p className="text-xs sm:text-sm text-gray-600">
+                    Qty: {item.quantity}
+                  </p>
+                  <p className="text-xs sm:text-sm text-gray-600">
                     Price: ${item.productId.price.toFixed(2)}
                   </p>
                 </div>
@@ -185,7 +251,7 @@ const UserOrders = () => {
           </ul>
 
           <button
-            onClick={closeModal}
+            onClick={closeDetailsModal}
             className="mt-6 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 w-full"
           >
             Close
---
+++ src/Admin/Allorders/CancelOrReturn/CancelOrReturn.jsx
@@ -0,0 +1,215 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { rejectCancelOrReturnRequest, updateOrderStatus } from "../../../app/Slice/adminSlices/ordersSlices/ordersThunk";
+import { useDispatch } from "react-redux";
+
+const CancelOrReturn = ({ order, currentPage, ordersPerPage }) => {
+  const dispatch = useDispatch();
+
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [modalType, setModalType] = useState(null);
+  const [selectedOrderForStatus, setSelectedOrderForStatus] = useState(null);
+  const [newStatus, setNewStatus] = useState("");
+
+  const openRequestModal = (order, type) => {
+    setSelectedOrder(order);
+    setModalType(type);
+  };
+
+  const closeRequestModal = () => {
+    setSelectedOrder(null);
+    setModalType(null);
+  };
+
+  const handleReject = () => {
+    dispatch(rejectCancelOrReturnRequest({orderId:selectedOrder.orderDetails.orderId,modalType}))
+    closeRequestModal();
+  };
+  const handleAllow = () => {
+    console.log(`Allow ${modalType} for Order ID: ${selectedOrder._id}`);
+    closeRequestModal();
+  };
+  const openStatusModal = (order) => {
+    setSelectedOrderForStatus(order);
+    setNewStatus(order.status);
+  };
+
+  const closeStatusModal = () => {
+    setSelectedOrderForStatus(null);
+    setNewStatus("");
+  };
+
+  const handleStatusUpdate = () => {
+    if (selectedOrderForStatus && newStatus) {
+      dispatch(
+        updateOrderStatus({
+          orderId: selectedOrderForStatus.orderId,
+          status: newStatus,
+          currentPage,
+          ordersPerPage,
+          dispatch,
+        })
+      )
+        .then(() => {
+          closeStatusModal();
+        })
+        .catch((err) => {
+          console.error("Failed to update status", err);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <td className="py-4 px-6 border-b border-gray-200">
+        {order.orderDetails.status === "Returned" &&
+        order.orderDetails.isCancelled ? (
+          <div className="flex items-center bg-red-50 text-red-700 border border-red-500 p-2 space-x-2 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 8v4m0 4h.01M21 12c0 4.97-4.03 9-9 9S3 16.97 3 12 7.03 3 12 3s9 4.03 9 9z"
+              />
+            </svg>
+            <button
+              className="font-mono text-sm underline"
+              onClick={() => openRequestModal(order, "return")}
+            >
+              Return Request Pending
+            </button>
+          </div>
+        ) : order.orderDetails.status === "Cancelled" &&
+          order.orderDetails.isCancelled ? (
+          <div className="flex items-center bg-yellow-50 text-yellow-700 border border-yellow-500 p-2 space-x-2 rounded-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-yellow-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 8v4m0 4h.01M21 12c0 4.97-4.03 9-9 9S3 16.97 3 12 7.03 3 12 3s9 4.03 9 9z"
+              />
+            </svg>
+            <button
+              className="font-mono text-sm underline"
+              onClick={() => openRequestModal(order, "cancel")}
+            >
+              Cancel Request Pending
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => openStatusModal(order.orderDetails)}
+            className="bg-rose-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-rose-700 transition duration-200"
+          >
+            Actions
+          </button>
+        )}
+      </td>
+
+      <Modal
+        isOpen={!!selectedOrder}
+        onRequestClose={closeRequestModal}
+        contentLabel={`${modalType === "return" ? "Return" : "Cancel"} Request`}
+        className="bg-white p-4 rounded-lg w-3/4 max-w-md mx-auto"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center"
+      >
+        <h2 className="text-2xl font-bold mb-4">
+          {modalType === "return"
+            ? "Allow or Reject Return Request"
+            : "Allow or Reject Cancellation Request"}
+        </h2>
+
+        <div className="mb-6 p-4 bg-gray-100 border-l-4 border-blue-600 rounded-md shadow-sm">
+          <p className="font-semibold text-gray-800">User's Reason:</p>
+          <p className="mt-1 text-gray-700">
+            {selectedOrder?.orderDetails.reason  || "No reason provided."}
+          </p>
+        </div>
+
+        <div className="flex space-x-4">
+          <button
+            onClick={handleReject}
+            className="bg-red-600 text-white rounded-md px-4 py-2 hover:bg-red-500 transition duration-200"
+          >
+            Reject
+          </button>
+          <button
+            onClick={handleAllow}
+            className="bg-green-600 text-white rounded-md px-4 py-2 hover:bg-green-500 transition duration-200"
+          >
+            Allow
+          </button>
+        </div>
+      </Modal>
+
+      {selectedOrderForStatus && (
+        <Modal
+          isOpen={!!selectedOrderForStatus}
+          onRequestClose={closeStatusModal}
+          ariaHideApp={false}
+          className="relative p-4 sm:p-8 bg-white rounded-lg shadow-lg mx-auto mt-4 sm:mt-10 max-w-full sm:max-w-2xl max-h-[96vh] overflow-y-auto"
+          overlayClassName="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center"
+        >
+          <h2 className="text-xl sm:text-2xl font-bold mb-4 sm:mb-6 text-gray-700 border-b pb-3">
+            Update Order Status
+          </h2>
+
+          <div className="space-y-4 text-sm sm:text-base">
+            <p>
+              <strong className="text-gray-600">Order ID:</strong>{" "}
+              {selectedOrderForStatus.orderId}
+            </p>
+            <div>
+              <label className="block text-gray-700 font-semibold mb-1">
+                Update Status:
+              </label>
+              <select
+                value={newStatus}
+                onChange={(e) => setNewStatus(e.target.value)}
+                className="w-full p-2 border rounded-md"
+              >
+                <option value="">Select</option>
+                <option value="Processing">Processing</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Cancelled">Cancelled</option>
+                <option value="Delivered">Delivered</option>
+                <option value="Returned">Returned</option>
+                <option value="Refunded">Refunded</option>
+              </select>
+            </div>
+          </div>
+
+          <button
+            onClick={handleStatusUpdate}
+            className="bg-indigo-600 text-white py-2 px-4 rounded hover:bg-indigo-700 mt-4"
+          >
+            Update Status
+          </button>
+
+          <button
+            onClick={closeStatusModal}
+            className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 mt-4 ml-4"
+          >
+            Close
+          </button>
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default CancelOrReturn;
